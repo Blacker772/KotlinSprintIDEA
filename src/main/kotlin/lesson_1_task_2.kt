3f975cc009c015f556ad0c772366f2341cd98bd1
Nurlan Veliev <133061421+Blacker772@users.noreply.github.com>
2024-03-20 20:32:18 +0400

    KS-1-2 print employees

---
+++ src/main/kotlin/lesson_1_task_2.kt
@@ -0,0 +1,14 @@
+package org.example
+
+fun main() {
+    val quantityOfOrders = 75
+    val notificationText = "Заказ принят! Ожидайте!"
+    println(quantityOfOrders)
+    println(notificationText)
+
+    var numberOfEmployees = 2000
+//    println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+}
