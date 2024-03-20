8ce327473793d6efa01e4a65f0051aaf6982d714
Nurlan Veliev <133061421+Blacker772@users.noreply.github.com>
2024-03-20 14:01:01 +0400

    KS-3-3 printing table

---
+++ src/main/kotlin/lesson_3_task_3.kt
@@ -0,0 +1,17 @@
+package org.example
+
+fun main() {
+    val number = 9
+    val arrayOfNumber = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+    println("$number x ${arrayOfNumber[0]} = ${number * arrayOfNumber[0]}\n" +
+            "$number x ${arrayOfNumber[1]} = ${number * arrayOfNumber[1]}\n" +
+            "$number x ${arrayOfNumber[2]} = ${number * arrayOfNumber[2]}\n" +
+            "$number x ${arrayOfNumber[3]} = ${number * arrayOfNumber[3]}\n" +
+            "$number x ${arrayOfNumber[4]} = ${number * arrayOfNumber[4]}\n" +
+            "$number x ${arrayOfNumber[5]} = ${number * arrayOfNumber[5]}\n" +
+            "$number x ${arrayOfNumber[6]} = ${number * arrayOfNumber[6]}\n" +
+            "$number x ${arrayOfNumber[7]} = ${number * arrayOfNumber[7]}\n" +
+            "$number x ${arrayOfNumber[8]} = ${number * arrayOfNumber[8]}"
+    )
+}
