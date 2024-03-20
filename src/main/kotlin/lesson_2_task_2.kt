bcd8c489c5339fe64ac582f32be1db8d151d70b9
Nurlan Veliev <133061421+Blacker772@users.noreply.github.com>
2024-03-20 12:42:04 +0400

    KS-2-2 refactoring

--- src/main/kotlin/lesson_2_task_2.kt
+++ src/main/kotlin/lesson_2_task_2.kt
@@ -1,22 +1,18 @@
 package org.example
 
-fun main(){
-
+fun main() {
     val employee = 50
     val employeeSalary = 30000
     val interns = 30
     val internsSalary = 20000
 
-    //расход на постоянных сотрудников
     val expensesEmployee = employee * employeeSalary
     println(expensesEmployee)
 
-    //общий расход по ЗП
     val expensesInterns = interns * internsSalary
     val generalExpenses = expensesEmployee + expensesInterns
     println(generalExpenses)
 
-    //среднее ЗП
-    val averageSalary = (expensesEmployee + expensesInterns)/(employee + interns)
+    val averageSalary = (expensesEmployee + expensesInterns) / (employee + interns)
     println(averageSalary)
 }
