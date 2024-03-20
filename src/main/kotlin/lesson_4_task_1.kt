bb4e6f7729bf5a627c74ca1c6bd4fc001d567457
Nurlan Veliev <133061421+Blacker772@users.noreply.github.com>
2024-03-20 18:26:30 +0400

    KS-4-1 показал доступность столиков

---
+++ src/main/kotlin/lesson_4_task_1.kt
@@ -0,0 +1,13 @@
+package org.example
+
+fun main() {
+    val reservedTablesToDay = 13
+    val reservedTablesTomorrow = 9
+
+    val freeTablesToDay = reservedTablesToDay != TOTAL_TABLES
+    val freeTablesTomorrow = reservedTablesTomorrow != TOTAL_TABLES
+
+    println("[Доступность столиков на сегодня:$freeTablesToDay]\n"+
+            "[Доступность столиков на сегодня:$freeTablesTomorrow]")
+}
+const val TOTAL_TABLES = 13
