fa599ce8302afd792d49beed0145d5ffe92fa178
Nurlan Veliev <133061421+Blacker772@users.noreply.github.com>
2024-03-20 20:39:51 +0400

    KS-1-2 print time

---
+++ src/main/kotlin/lesson_1_task_5.kt
@@ -0,0 +1,14 @@
+package org.example
+
+fun main() {
+    val totalSecondsInSpace = 6480
+    val secondInHour = 3600
+    val secondInMinutes = 60
+
+    val hours = totalSecondsInSpace / secondInHour
+    val minutes = (totalSecondsInSpace % secondInHour) / secondInMinutes
+    val seconds = totalSecondsInSpace % secondInMinutes
+
+    val formatTime = "%02d:%02d:%02d".format(hours, minutes, seconds)
+    println(formatTime)
+}
