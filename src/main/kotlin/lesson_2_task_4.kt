083630863ab32e6e145b17212bbc139594da2de4
Nurlan Veliev <133061421+Blacker772@users.noreply.github.com>
2024-03-20 13:15:41 +0400

    KS-2-4 refactoring

--- src/main/kotlin/lesson_2_task_4.kt
+++ src/main/kotlin/lesson_2_task_4.kt
@@ -1,12 +1,13 @@
 package org.example
 
-fun main(){
+fun main() {
     val buff = 20
     val crystal = 7
     val ironOre = 11
+    val percent = 100.0
 
-    val crystalBuff = (crystal/100.0) * buff
-    val ironOreBuff = (ironOre/100.0) * buff
+    val crystalBuff = (crystal / percent) * buff
+    val ironOreBuff = (ironOre / percent) * buff
 
     println("Кристалов добыто: $crystal, с баффом: ${crystalBuff.toInt()}")
     println("Железной руды добыто: $ironOre, с баффом: ${ironOreBuff.toInt()}")
